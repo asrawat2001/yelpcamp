07731defacde3bfc0e41fabe45a69fdcf9eb6570
Ankit Singh Rawat <83879728+asrawat2001@users.noreply.github.com>
2022-09-09 19:36:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -87,6 +87,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
+});
