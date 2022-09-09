94172ca8a4ca828a4ea18d9a92d7cc3040816378
Ankit Singh Rawat <83879728+asrawat2001@users.noreply.github.com>
2022-09-09 20:04:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-
+var mongoose = require('./mongoconnection');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
